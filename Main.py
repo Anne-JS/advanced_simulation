9694ee73b38ce7413f230638fd61ab5d50adc6cd
JulianvanRie <148776744+JulianvanRie@users.noreply.github.com>
2024-02-23 16:20:11 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ Output will be BMMS_overiew_new.xlsx and _roads_new.tsv
 """
 
 def main():
-    bmms, df_rds, df_road_ranges, df_rds_restructured = F.clean('BMMS_overview.xlsx', '_roads.tsv')
+    bmms, df_rds, df_road_ranges, df_rds_restructured = F.clean(input/'BMMS_overview.xlsx', input/'_roads.tsv')
     F.make_files(bmms, df_rds)
     return bmms, df_rds, df_road_ranges, df_rds_restructured
 
